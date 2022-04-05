54453cb416b60a8045aaf8ee8f170cc5781695a0
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-04-05 14:31:34 +0200

    added authentication

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -10,7 +10,9 @@ export default {
       areas: data.areas
     };
 
-    const response = await fetch(`https://testcoac-55081-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`, {
+    const token = context.rootGetters.token;
+
+    const response = await fetch(`https://testcoac-55081-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json?auth=` + token, {
       method: 'PUT',
       body: JSON.stringify(coachData)
     });
